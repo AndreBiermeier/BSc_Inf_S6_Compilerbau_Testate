509c958a855954536a1aa9aa20a2615ecb75e347
AndreBiermeier <andre.biermeier@studmail.htw-aalen.de>
2026-01-14 21:55:27 +0000

    comment describing the script

--- toggle_ast.sh
+++ toggle_ast.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script toggles between the normal ast and the ast printing expression nodes.
+# All it does is to copy the content of the corresponding ast_* into ast.h and ast.cpp.
+
 cd ast || exit 1
 
 if grep -q "EXPRESSION_AST" ast.h 2>/dev/null; then
